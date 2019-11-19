232c9879048a16711e6de56b5f917526b12c7bf0
EchEdward <e.yotch@yandex.by>
2019-11-19 22:25:46 +0300

    create express app (edited mistakes)

--- public/js/Carousel.js
+++ public/js/Carousel.js
@@ -1,7 +1,7 @@
 // Carousel Auto-Cycle
 $(document).ready(function() {
   if ($("#content-anchor").length) {
-    $('body,html').animate({scrollTop: $("#content-anchor").offset().top-100}, 0);
+    $('body,html').animate({scrollTop: Math.round($("#content-anchor").offset().top)-95}, 0);
   }
   $('#carouselExampleCaptions').carousel({
     interval: 5000
